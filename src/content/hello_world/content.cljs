07f2fc3d4808cbe5ae3234b396806eddccc6ac33
Steve Chan <chanshunli@gmail.com>
2018-05-26 09:24:15 +0800

    将快捷键用在Chrome插件里面: 测试OK了

--- src/content/hello_world/content.cljs
+++ src/content/hello_world/content.cljs
@@ -2,17 +2,47 @@
 
 (enable-console-print!)
 
-(println "Hello from cljs, This is content JS")
+(println "Hello from cljs, This is content JS !")
 
 ;; in brower environment?
 (when (.-body js/document)
-  (set! (.. js/document
-            -body
-            -style
-            -backgroundColor)
-        "green")
+  (set!
+   js/window.onkeydown
+   (fn [e]
+     (let [keycode (.-keyCode e)
+           ;; 0~9 => 48~57
+           ctrlkey (.-ctrlKey e)
+           ;; true or false
+           metakey (.-metaKey e)]
+       ;; Ctrl的组合键
+       (if (and ctrlkey (not= keycode 17))
+         (cond
+           ;; 数字键
+           ((set (range 47 58)) keycode)
+           (js/alert "这里是数字键!")
+           ;;
+           (= 71 keycode)
+           (let [selector (.getSelection js/window)
+                 select-stri (str (.toString selector))]
+             (js/alert (str "正在谷歌搜索:" select-stri))
+             )
+           (= 83 keycode)
+           (prn 1111111)
+           ;;
+           :else (prn keycode))
+         nil)
+       )
+     )
+   )
 
-  (set! (.. js/document
-            -body
-            -innerHTML)
-        "<h1 align='center'>You got an extension written in ClojureScript!😄</h1>"))
+  #_(set! (.. js/document
+              -body
+              -style
+              -backgroundColor)
+          "green")
+
+  #_(set! (.. js/document
+              -body
+              -innerHTML)
+          "<h1 align='center'>You got an extension written in ClojureScript!😄</h1>")
+  )
