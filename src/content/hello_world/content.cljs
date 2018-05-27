a41cba75f882c9d4796af1e04862816c1070501c
Steve Chan <chanshunli@gmail.com>
2018-05-27 20:35:47 +0800

    将Google搜索的标签放入任意网页: jimw-clj搜索也可以放入

--- src/content/hello_world/content.cljs
+++ src/content/hello_world/content.cljs
@@ -7,39 +7,61 @@
 
 (enable-console-print!)
 
+(def google-input-html
+  "<form target=\"_blank\" action=\"http://www.google.com/search\" method=\"get\"><input type=\"text\" id=\"google-input\" name=\"q\"><input type=\"submit\" value=\"Google\" id=\"google-input-button\"></form>")
+
+(defn body-append-html-stri [html]
+  (.appendChild
+   (.-body
+    js/document)
+   (-> (js/DOMParser.)
+       (.parseFromString
+        html "text/html")
+       .-body
+       .-firstElementChild)))
+
 (println "Hello from cljs, This is content JS !")
 
 ;; in brower environment?
 (when (.-body js/document)
-  (set!
-   js/window.onkeydown
-   (fn [e]
-     (let [keycode (.-keyCode e)
-           ;; 0~9 => 48~57
-           ctrlkey (.-ctrlKey e)
-           ;; true or false
-           metakey (.-metaKey e)]
-       ;; Ctrl的组合键
-       (if (and ctrlkey (not= keycode 17))
-         (cond
-           ;; 数字键
-           ((set (range 47 58)) keycode)
-           (js/alert "这里是数字键!")
-           ;;
-           (= 71 keycode)
-           (let [selector (.getSelection js/window)
-                 select-stri (str (.toString selector))]
-             (js/alert (str "正在谷歌搜索:" select-stri))
-             )
-           (= 83 keycode)
-           (prn 1111111)
-           ;;
-           :else (prn keycode))
-         nil)
+  (do
+    ;; A: Emacs的钢琴键组合思想:纯函数组合
+    (set!
+     js/window.onkeydown
+     (fn [e]
+       (let [keycode (.-keyCode e)
+             ;; 0~9 => 48~57
+             ctrlkey (.-ctrlKey e)
+             ;; true or false
+             metakey (.-metaKey e)]
+         ;; Ctrl的组合键
+         (if (and ctrlkey (not= keycode 17))
+           (cond
+             ;; 数字键
+             ((set (range 47 58)) keycode)
+             (js/alert "这里是数字键!")
+             ;;
+             (= 71 keycode)
+             (let [selector (.getSelection js/window)
+                   select-stri (str (.toString selector))]
+               (js/alert (str "正在谷歌搜索:" select-stri))
+               )
+             (= 83 keycode)
+             (prn 1111111)
+             ;;
+             :else (prn keycode))
+           nil)
+         )
        )
      )
-   )
-
+    ;; B:Google的搜索回归: 关键词对应答案或者树 => jimw-clj搜索也可以放入
+    (do
+      ;; Github: Refused to send form data to '<URL>' because it violates the following Content Security Policy directive: "form-action 'self' github.com gist.github.com".
+      ;; Stackoverflow: OK,append之后还可以搜索
+      (body-append-html-stri google-input-html)
+      
+      )
+    )
   #_(set! (.. js/document
               -body
               -style
