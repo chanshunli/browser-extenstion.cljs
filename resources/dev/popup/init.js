626ac42d59aa3e7e4c75aa2d9876484ff1e28401
Steve Chan <chanshunli@gmail.com>
2018-05-29 14:56:34 +0800

    http://www.cnblogs.com/dudu/archive/2012/11/22/chrome_extension.html

---
+++ resources/dev/popup/init.js
@@ -0,0 +1,8 @@
+   function login(usr, pwd){
+       console.log(usr);
+       console.log(pwd);
+   };
+   document.getElementById('login-bt').onclick = function () {
+       login(11111111,2222);
+   };
+
