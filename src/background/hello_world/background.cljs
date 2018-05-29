4737c370d442419df82b110c70e22a1a4c55c259
Steve Chan <chanshunli@gmail.com>
2018-05-29 16:06:46 +0800

    goog.require could not find: figwheel.connect.build_dev goog.logToConsole_ @ base.js:677 base.js:711 Uncaught Error: goog.require could not find: figwheel.connect.build_dev     at Object.goog.require (base.js:711)     at init.js:2

--- src/background/hello_world/background.cljs
+++ src/background/hello_world/background.cljs
@@ -8,4 +8,19 @@
 (when (goog.object/getValueByKeys js/window "chrome" "browserAction")
   (.addListener js/chrome.browserAction.onClicked
                 (fn [_]
-                  (js/alert "Write browser extension in ClojureScript!"))))
+                  (js/alert "Write browser extension in ClojureScript!")))
+  #_(.addListener js/chrome.runtime.onMessage
+                  (fn [msg sender send-response]
+                    (println (str "listen message done~~~" (js->clj msg :keywordize-keys true)))
+                    true))
+  #_(println "listen request done!")
+  #_(.addListener js/chrome.runtime.onMessage
+                  (fn [msg sender send-response]
+                    (prn 111111111)
+                    #_(match [(js->clj msg :keywordize-keys true)]
+                             [{:sandbox test-url}] (send-response (modify-url test-url))
+                             [{:url goto-url}] (.create js/chrome.tabs (clj->js {:url goto-url}))
+                             [msg] (println "Unknown: " msg))
+                    ;; https://developer.chrome.com/extensions/runtime#event-onMessage
+                    true))
+  )
